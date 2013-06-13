81993352a8e19481d228884efc133e465e742271
Annie Zeng <aqunzeng@gmail.com>
2013-06-12 23:52:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZengPortfolio::Application.config.secret_token = '689981c5878a39c09255bb8c73e01ea37fd1acf4e608c8780bf87da70e130ce078ccea7fe3947d7795521eda53eb49fd9b53e6eb269e21279ea16a49a03a8f1f'
